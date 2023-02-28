f5275252e6fcc87b0db46f3ed55a16e57d0ba3f6
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-02-28 12:41:23 -0300

    create first theme using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import "./global.css"
+import { ThemeProvider } from "styled-components";
+
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
+import "./global.css"
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button variant="danger"/>
       <Button />
-    </>
+    </ThemeProvider>
   );
 }
