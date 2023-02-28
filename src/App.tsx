cfcf0cbef5575bbcdf2e09c43e9b4015a85ad044
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-02-28 14:13:51 -0300

    create Home and History routes using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
-import { Button } from './components/Button'
-import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
