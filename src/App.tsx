a0eb6c5bff3d6b984e812c689c8c1c3eed571b6c
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-02 18:58:08 -0300

    learning about the Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { ThemeProvider } from 'styled-components'
-import { BrowserRouter } from 'react-router-dom'
+// import { ThemeProvider } from 'styled-components'
+// import { BrowserRouter } from 'react-router-dom'
 
-import { Router } from './Router'
+import { Home } from './Home'
 
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+// import { Router } from './Router'
+
+// import { GlobalStyle } from './styles/global'
+// import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-        <GlobalStyle />
-      </BrowserRouter>
-    </ThemeProvider>
+    // <ThemeProvider theme={defaultTheme}>
+    //   <BrowserRouter>
+    //     <Router />
+    //     <GlobalStyle />
+    //   </BrowserRouter>
+    // </ThemeProvider>
+    <Home />
   )
 }
